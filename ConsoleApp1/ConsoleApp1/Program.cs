aa710594cd63ef3b0c6dd4f563a9f3275e16c8fa
Богдан К <epam.2@mail.ru>
2022-10-18 15:20:41 +0400

    first commit

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,39 @@
+﻿//Программа принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number, firstNumber, secondNumber;
+            string input;
+            bool IsTrue;
+
+
+            input = Console.ReadLine();
+            IsTrue = int.TryParse(input, out number);
+
+            while (IsTrue == false || number < 100 || number > 999)
+            {
+                Console.WriteLine("Число должно быть целым и трёхзначным");
+                Console.Write("Повторите ввод: ");
+                input = Console.ReadLine();
+                IsTrue = int.TryParse(input, out number);
+            }
+
+            firstNumber = number / 10;  //Рассчитываем первое число
+            secondNumber = firstNumber % 10;  //Рассчитываем второе число
+
+            Console.WriteLine($"Второе число {secondNumber}");
+
+            Console.ReadKey();
+        }
+    }
+}
