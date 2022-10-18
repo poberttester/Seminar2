665ed18821c8cd39c7e57159737cbd5798bb5cec
Богдан К <epam.2@mail.ru>
2022-10-18 17:11:26 +0400

    Add projects 'ConsoleApp2 & ConsoleApp3

---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,52 @@
+﻿//Программа выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int result, separeteNumber = 0;
+            string input;
+            bool IsTrue;
+
+
+            input = Console.ReadLine();
+            IsTrue = int.TryParse(input, out int number);
+
+
+            while (IsTrue == false)     // проверка, что данные ведённые пользователем являются числом int
+            {
+                Console.WriteLine("Число должно быть в диапазоне integer");
+                Console.Write("Повторите ввод: ");
+                input = Console.ReadLine();
+                IsTrue = int.TryParse(input, out number);
+            }
+
+
+            if (number < 100)    // проверка, что число является трёхзначным
+            {
+                Console.WriteLine("Третьего числа нет");
+                goto link;
+            }
+
+            
+            for (int temp = number; temp > 99; temp /= 10)  // уменьшаем число до трёхзначного
+            {                
+                separeteNumber = temp;
+            }
+
+            result = separeteNumber % 10;
+            Console.WriteLine($"Третья цифра заданного числа: {result}");
+
+           link:
+                Console.ReadKey();
+        }
+    }
+}
---
+++ ConsoleApp3/ConsoleApp3/Program.cs
@@ -0,0 +1,66 @@
+﻿// Программа принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            byte result;
+            string input;
+            bool IsTrue;
+
+
+            input = Console.ReadLine();
+            IsTrue = byte.TryParse(input, out byte day);
+
+
+            while(IsTrue == false || day < 1 || day > 7)
+            {
+                Console.Write("Необходимо ввести число от 1 до 7.\nПовторите ввод: ");
+                input = Console.ReadLine();
+                IsTrue = byte.TryParse(input, out day);
+            }
+
+
+            switch (day)
+            {
+                case 1:
+                    Console.WriteLine("Понедельник - день тяжёлый");
+                    break;
+
+                case 2:
+                    Console.WriteLine("Вторник - день рабочий");
+                    break;
+
+                case 3:
+                    Console.WriteLine("Среда - середина рабочей недели");
+                    break;
+
+                case 4:
+                    Console.WriteLine("Четверг - четвёртый рабочий день");
+                    break;
+
+                case 5:
+                    Console.WriteLine("Пятница - последний рабочий день");
+                    break;
+
+                case 6:
+                    Console.WriteLine("Суббота - выходной день");
+                    break;
+
+                case 7:
+                    Console.WriteLine("Воскресенье - выходной день");
+                    break;
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
